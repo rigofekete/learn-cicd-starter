e4ce74b9aec2c2421802733f00368a1e57a98e59
rigofekete <fabricecosteira@gmail.com>
2026-06-06 11:14:31 +0200

    test: commit failing test to confirm CI fails

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := http.Header{
+		"Authorization": []string{"ApiKey fa234bbaff494"},
+	}
+
+	key, err := GetAPIKey(headers)
+	require.NoError(t, err)
+	assert.Equal(t, key, "fa234bbaff494_G")
+}
+
+func TestGetAPIKey_Fail(t *testing.T) {
+	headers := http.Header{
+		"Authorization": []string{"Bearer fa234bbaff494"},
+	}
+
+	_, err := GetAPIKey(headers)
+	require.Error(t, err)
+}
