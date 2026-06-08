cf713cf2622bf2259646718a8cd5a81317b50135
rigofekete <fabricecosteira@gmail.com>
2026-06-08 22:50:46 +0200

    refactor: check port numeric value and handle error. add ReadHeaderTimeout value to srv initialization

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -33,6 +35,9 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+	if _, err := strconv.Atoi(port); err != nil {
+		log.Fatal("invalid port")
 	}
 
 	apiCfg := apiConfig{}
@@ -89,8 +94,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: time.Second * 2,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
